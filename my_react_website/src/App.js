8f3662523a89a0611c02d02b3e28483c0eb6679f
Christian Villalba UK <christianvillalba.uk@gmail.com>
2021-12-08 14:05:20 +0000

    adds routes, uses switch. Path /home working

--- my_react_website/src/App.js
+++ my_react_website/src/App.js
@@ -1,14 +1,21 @@
 import React, {Component} from "react";
 import PageWrapper from "./components/PageWrapper";
 import Home from "./components/Pages/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
+class App extends Component {
+
+  render() {
+    return (
     <PageWrapper >
-    <Home/>
-    <h1>Cristian made this</h1>
+      <Router>
+      <Switch>
+          <Route path="/home" component={Home} />
+        </Switch>
+      </Router>
     </PageWrapper>
-  );
+    );
+  }
 }
 
 export default App;
