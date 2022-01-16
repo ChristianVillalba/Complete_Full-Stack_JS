daf2fb17197f628b2f7abf535944b4baaa75dad7
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-01-16 18:29:05 +0000

    database populated using the commented script. Data created, and saved using toSave function. Operative

--- websitebackend/server/test.js
+++ websitebackend/server/test.js
@@ -3,13 +3,28 @@
 // Import models
 var models = require("./server.js").models;
 
-models.Profile.findOrCreate({name: "Chris2"}, (err, profile) => {
-    if(err){
-        console.log("Tere was an error", err);
-    }else if(profile){
-        profile.email= "chris@chrisvilla2.com";
-        profile.save((ue, updated) => {
-            console.log("Updated?", updated);
-        });
-    }
-})
+// POPULATING OUR DATABASE:
+// var toSave = [
+//     {"name": "Cris5", "email" : "cris5@cris.com"},
+//     {"name": "No Cris", "email" : "cris99@cris.com"},
+//     {"name": "Totally No Cris", "email" : "cris99@cris.com"},
+//     {"name": "Cris but no the one you know", "email" : "cris999@cris.com"},
+//     {"name": "Cris5", "email" : "cris5@cris.com"},
+//     {"name": "Qwer", "email" : "qwer@cris.com"},
+//     {"name": "Asdf", "email" : "asdf@cris.com"},
+//     {"name": "Zxcv", "email" : "zxcv@cris.com"},
+//     {"name": "Uiop", "email" : "uiop@cris.com"},
+//     {"name": "Jklh", "email" : "jklh@cris.com"},  
+//     {"name": "Tygh", "email" : "tygh@cris.com"},
+// ];
+
+// toSave.map(obj => {
+//     models.Profile.create(obj, (err, created) => {
+//         console.log("Created? ", created );
+//     })
+// })
+
+// git commit -m "" :
+// database populated using the commented script. Data created, and saved using toSave function. Operative
+
+models.Profile.findOne({})
