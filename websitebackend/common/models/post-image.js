c0b793a970139c80ae76b75148bbcba28e358780
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-01-23 22:09:41 +0000

    adds sharp library using npm. edits post-image.js. creating parameters adn importing shapr library

--- websitebackend/common/models/post-image.js
+++ websitebackend/common/models/post-image.js
@@ -1,5 +1,18 @@
 'use strict';
+const sharp = require("sharp");
 
-module.exports = function(Postimage) {
+module.exports = function(PostImage) {
+    PostImage.remoteMethod(
+        "upload",
+        {
+            description: "Uploads file",
+            accepts: [
+                { arg: "ctx", type: "object", http: {source: "context"}},
+                { arg: "options", type: "object", http: {source: "query"}},
+                { arg: "access_token", type: "string"},
+                { arg: "post_id", type: "string"}
+            ],
+        }
+    )
 
 };
