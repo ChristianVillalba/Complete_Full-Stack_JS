ae04313ce08fc79456d1473368fbd6bfd5e8e604
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-02 17:48:36 +0000

    adds material-ui. separates AdminWrapper and LogginWrapper

--- my_react_website/src/components/AdminWrapper.js
+++ my_react_website/src/components/AdminWrapper.js
@@ -1,10 +1,15 @@
 import React, {Component} from "react";
 import "./assets/css/admin.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 class AdminWrapper extends Component {
     render() {
         return (
             <div id="admin-page">
+                <AppBar>
+                    <ToolBar />
+                </AppBar>
                 {this.props.children}
             </div>
         )
