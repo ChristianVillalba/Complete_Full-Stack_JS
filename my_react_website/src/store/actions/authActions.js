98ce48368be97e1cc9b42f79c0bfecff70528206
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-02 16:36:26 +0000

    adds tokens using redux and the redux-thung library

--- my_react_website/src/store/actions/authActions.js
+++ my_react_website/src/store/actions/authActions.js
@@ -4,10 +4,14 @@ export const login = (email, pass) => {
     return (dispatch) => {
         API.login(email, pass, res => {
             console.log("Result", res.data);
-            return {
+            dispatch ({
                 type: "LOGIN",
-                payload: {email, pass}
-            }
+                payload: {
+                    email: email, 
+                    token: res.data.id,
+                    userId: res.data.userId
+                }
+            })
         })
     }
 
