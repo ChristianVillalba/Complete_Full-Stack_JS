8106a6176232cd440faafdd86287ae2bbbabf863
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-03 08:37:55 +0000

    adds material-ui/icons and import new Components to AdminWrapper.js

--- my_react_website/src/components/AdminWrapper.js
+++ my_react_website/src/components/AdminWrapper.js
@@ -4,13 +4,27 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
+//  Drawer imports
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// import Divider from '@material-ui/core/Divider';
+
 class AdminWrapper extends Component {
     render() {
         return (
             <div id="admin-page">
                 <AppBar>
-                    <Toolbar component="h1" variant="h6" color="inherit" noWrap>
-                        Admin
+                    <Toolbar>
+                        <Typography
+                            component="h1"
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                            >Admin</Typography>
                     </Toolbar>
                 </AppBar>
                 {this.props.children}
