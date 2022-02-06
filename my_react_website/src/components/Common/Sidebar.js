71b40e88207ffc064380d2780ec54c06e752e981
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-06 21:15:06 +0000

    adds functional links to the elements in Sidebar.js

--- my_react_website/src/components/Common/Sidebar.js
+++ my_react_website/src/components/Common/Sidebar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-// import {Link as RouterLink} from 'react-router-dom';
-// import Link from '@material-ui/core/Link';
+import {Link as RouterLink} from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -10,34 +10,38 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import FaceIcon from '@material-ui/icons/Face';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import Dashboard from '@material-ui/icons/Dashboard';
-// import FileCopyIcon from '@material-ui/icons/FileCopy';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+
+function ListItemLink(props) {
+    return <ListItem button component={RouterLink} {...props} />;
+  }
 
 class Sidebar extends Component {
     render(){
         return (
-            <List>
-                <ListItem button>
-                    <ListItemIcon>
-                        <DashboardIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Dashboard"/> 
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <DashboardIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Posts"/> 
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <FaceIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Users"/> 
-                </ListItem>
 
+            <List>
+                <ListItemLink to="/admin">
+                        <ListItemIcon>
+                            <DashboardIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Dashboard" />
+                </ListItemLink>
+
+                <ListItemLink  to="/admin/posts">
+                        <ListItemIcon>
+                            <FileCopyIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Posts" />
+                </ListItemLink>
+
+                <ListItemLink  to="/admin/users">
+                        <ListItemIcon>
+                            <FaceIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Users" />
+                </ListItemLink>
             </List>
         )
     }
 }
-
-export default Sidebar;
