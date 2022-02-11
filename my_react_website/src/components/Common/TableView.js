a7b9d03182c52b1a37ecc7aad6870aaa844108b7
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-11 22:12:33 +0000

    adds TableViews.js and import the elements that will be used

---
+++ my_react_website/src/components/Common/TableView.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {withStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+
+class TableView extends Component {
+    render(){}
+}
+
+export default TableView;
