58bdfa9f9452c5cd0b72a5452d561f3cc651f79e
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-13 22:17:27 +0000

    adds AdminActions.js and AdminReducer.js

---
+++ my_react_website/src/store/reducers/adminReducer.js
@@ -0,0 +1,18 @@
+const defaultState = {
+    user: [],
+    posts: []
+}
+
+const admin = (state = defaultState, action) => {
+    switch(action.type){
+        case "GOT_USERS":
+            return {
+                ...state,
+                user: action.payload
+            }
+        default:
+            return state
+    }
+}
+
+export default admin;
