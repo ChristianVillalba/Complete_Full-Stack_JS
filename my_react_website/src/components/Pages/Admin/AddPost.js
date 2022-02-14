7b1ed81c4a4cf793067c9389b576fd0acbc12e72
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-14 02:34:32 +0000

    adds to AddPost.js , imports, styles and render()

--- my_react_website/src/components/Pages/Admin/AddPost.js
+++ my_react_website/src/components/Pages/Admin/AddPost.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as AdminActions from '../../../store/actions/adminActions';
+import Paper from '@material-ui/core/Paper';
 import {withStyles} from '@material-ui/core/styles';
+import * as Yup from 'yup';
 
-class AddPost extends Component {}
+const styles = theme => ({
+    container: {
+        margin: theme.spacing.unit * 3,
+        display: 'flex',
+        flexDirection: 'row wrap',
+        width: '100%'
+    },
+})
+
+
+class AddPost extends Component {
+    render(){
+        const {classes} = this.props;
+        return (
+            <div className={classes.container}>
+                <h1>Add Post</h1>
+            </div>
+        )
+    }
+}
 
 export default connect(
     // mapStateToProps,
