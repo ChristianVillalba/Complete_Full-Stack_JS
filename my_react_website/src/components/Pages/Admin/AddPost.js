14f4c21af61d3e6d26c56d6c7652011fce0d3cc3
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-14 02:46:30 +0000

    adds export to AddPost.js. It was messy and it was hard

--- my_react_website/src/components/Pages/Admin/AddPost.js
+++ my_react_website/src/components/Pages/Admin/AddPost.js
@@ -4,6 +4,7 @@ import * as AdminActions from '../../../store/actions/adminActions';
 import Paper from '@material-ui/core/Paper';
 import {withStyles} from '@material-ui/core/styles';
 import * as Yup from 'yup';
+import { withFormik } from 'formik';
 
 const styles = theme => ({
     container: {
@@ -26,7 +27,42 @@ class AddPost extends Component {
     }
 }
 
-export default connect(
-    // mapStateToProps,
-    // mapDispatchToProps
-)(Users);
+const mapStateToProps = state => ({
+    auth: state.auth,
+    admin: state.admin
+});
+
+const mapDispatchToProps = state => ({
+
+});
+
+
+export default withRouter(connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withFormik({
+    mapPropsToValues: (props) => ({
+        title: props.admin.post.title || '',
+        slug: props.admin.post.slug || '',
+        createdAt: props.admin.post.createdAt || '',
+        status: props.admin.post.status || false,
+        content: props.admin.post.content || ''
+    }),
+    validationSchema: Yup.object().shape({
+        title: Yup.string().required('Title is required'),
+        slug: Yup.string().required(),
+        content: Yup.string().required()
+    }),
+    handleSubmit: (values, {setSubmitting, props}) => {
+        console.log("Saving", props.addPost);
+        if(props.match.params.view === 'edit'){
+            const post = {
+                ...values,
+                id: props.match.params.id
+            }
+            props.updatePost(post, props.auth.token);
+        }else{
+            props.addPost(values, props.auth.token);
+        }
+    }
+})(withStyles(styles)(AddPost))));
