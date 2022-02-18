21a0b6197ca0e467a16dec92025a61af45da878f
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-18 21:16:21 +0000

    adds new routes to App.js. resize img in post-img.js

--- my_react_website/src/App.js
+++ my_react_website/src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { connect } from "react-redux";
 
 // Pages
-import Home from "./components/Pages/Home";
-import About from "./components/Pages/About";
-import Services from "./components/Common/Services";
-import Portfolio from "./components/Common/Portfolio";
-import Team from "./components/Common/Team";
-import Contact from "./components/Pages/Contact";
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import Contact from './components/Pages/Contact';
+import Login from './components/Pages/Login';
+import Blog from './components/Pages/Blog';
+import Single from './components/Pages/Single';
+// import Signup from './components/Pages/Signup';
 import Login from "./components/Pages/Login";
 
 // Admin Pages
@@ -110,19 +111,53 @@ class App extends Component {
              <Home {...props} />
             </PageWrapper>)}  
         />
-        <Route path="/about" render={props => (
-            <PageWrapper>
-             <About {...props} />
-            </PageWrapper>)}  
-        />
-        {/* <Route path="/services" component={Services}/>
-        <Route path="/portfolio" component={Portfolio}/>
-        <Route path="/team" component={Team}/> */}
-        <Route path="/contact" render={props => (
-            <PageWrapper>
-             <Contact {...props} />
-            </PageWrapper>)}  
-        />
+        
+        <Route
+              exact={true}
+              path="/"
+              render={props => (
+                <PageWrapper>
+                  <Home {...props} />
+                </PageWrapper>
+              )}
+            />
+
+            <Route
+              path="/blog/:slug"
+              exact={true}
+              render={props => (
+                <PageWrapper>
+                  <Single {...props} />
+                </PageWrapper>
+              )}
+            />
+
+            <Route
+              path="/blog"
+              exact={true}
+              render={props => (
+                <PageWrapper>
+                  <Blog {...props} />
+                </PageWrapper>
+              )}
+            />
+            <Route
+              path="/about"
+              render={props => (
+                <PageWrapper>
+                  <About {...props} />
+                </PageWrapper>
+              )}
+              />
+            
+            <Route 
+              path="/contact"
+              render={props => (
+                <PageWrapper>
+                  <Contact {...props} />
+                </PageWrapper>
+              )}
+              />
       
       </Router>
     );
