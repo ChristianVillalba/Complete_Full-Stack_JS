bbae5ccf3e3793bcab49a8d9c3d7c71a502677fd
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-02-22 09:05:19 +0000

    adds basic structure to Single.js

--- my_react_website/src/components/Pages/Admin/Single.js
+++ my_react_website/src/components/Pages/Admin/Single.js
@@ -1,2 +1,27 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 
+class Single extends Component {
+    render(){
+        return(
+            <h1>Single</h1>
+        )
+    }
+
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    site: state.site,
+})
+
+const mapDispatchToProps = dispatch => ({
+    getSinglePost: (slug, token) => {
+        dispatch (SiteActions.getPostBySlug(slug, token))
+    }
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Single);
