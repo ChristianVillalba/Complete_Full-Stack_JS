77a22e99521d75b4762db1600bca9e2ae2a291b8
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-03-02 12:57:16 +0000

    adds form (Field Component) to add comments

--- my_react_website/src/components/Common/CommentBuilder.js
+++ my_react_website/src/components/Common/CommentBuilder.js
@@ -3,11 +3,43 @@ import * as SiteActions from '../../store/actions/siteActions';
 import {connect} from 'react-redux';
 import {withFormik} from 'formik';
 import * as Yup from 'yup';
+import Field from './Field';
+
+
+const fields = [
+    {name: 'content', 
+    elementName: 'input', 
+    type: 'text', 
+    placeholder: 'Write your comment'}
+]
 
 class CommentBuilder extends Component {
     render(){
-        return(
-            <h1>Hi</h1>
+        return (
+            <div>
+                <form onSubmit={e => {
+                    e.preventDefault();
+                    const comment = {
+                        postId: this.props.site.post.id,
+                        profileId: this.props.auth.profile.id,
+                        userId: this.props.auth.user.id,
+                        content: this.props.values.content
+                    }
+                    this.props.postComment(comment, this.props.auth.token);
+                }}>
+
+                    <Field
+                        {...fields[0]}
+                        value={this.props.values[fields[0].name]}
+                        name={fields[0].name}
+                        onChange={this.props.handleChange}
+                        onBlur={this.props.handleBlur}
+                        touched={(this.props.touched[fields[0].name])}
+                        errors={this.props.errors[fields[0].name]}
+                    />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+                </div>
         )
     }
 }
